df2478269f88977c9957c9236ebc0d1f8846ea1b
Wonjaegang <robinsam@naver.com>
2021-05-13 23:38:05 +0900

    Add classes : Demonstrator & Settings
    - class variable 'population' randomly choose 100(populationSize from settings) numbers from 0 to 1. And if it is smaller then 0.1(prior from settings), save 1. Else, save 0.

--- BayesRule_Demonstrator.py
+++ BayesRule_Demonstrator.py
@@ -1,5 +1,29 @@
 import random
 
 
+class Demonstrator:
+    def __init__(self):
+        self.population = [1 if random.random() < settings.priorProbability else 0
+                           for _ in range(settings.populationSize)]
+        self.display()
+
+    def display(self):
+        print(self.population)
+        print("Infected : %d" % self.population.count(1))
+        print("Uninfected : %d" % self.population.count(0))
+
+
+class Settings:
+    def __init__(self):
+        self.populationSize = 100
+        self.priorProbability = 0.1
+        self.sensitivity = 0.9
+        self.specificity = 0.8
+
+
 if __name__ == "__main__":
+    settings = Settings()
+
+    demonstrator = Demonstrator()
+
     pass
