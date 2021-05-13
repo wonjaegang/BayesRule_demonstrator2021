25035c58921a53ee5853921c2276e18ba5bc9add
Wonjaegang <robinsam@naver.com>
2021-05-14 01:11:06 +0900

    Add class methods: spreadDisease & diagnosticTest
    - I added 2 methods to Demonstrator class. Both of them are called when instance is initialized.

--- BayesRule_Demonstrator.py
+++ BayesRule_Demonstrator.py
@@ -1,18 +1,6 @@
 import random
 
 
-class Demonstrator:
-    def __init__(self):
-        self.population = [1 if random.random() < settings.priorProbability else 0
-                           for _ in range(settings.populationSize)]
-        self.display()
-
-    def display(self):
-        print(self.population)
-        print("Infected : %d" % self.population.count(1))
-        print("Uninfected : %d" % self.population.count(0))
-
-
 class Settings:
     def __init__(self):
         self.populationSize = 100
@@ -21,9 +9,34 @@ class Settings:
         self.specificity = 0.8
 
 
+class Demonstrator:
+    def __init__(self):
+        self.infected = 0
+        self.positive = 0
+        self.positive_infected = 0
+        self.spreadDisease()
+        self.diagnosticTest()
+
+    def spreadDisease(self):
+        for _ in range(settings.populationSize):
+            if random.random() < settings.priorProbability:
+                self.infected += 1
+
+    def diagnosticTest(self):
+        for _ in range(self.infected):
+            if random.random() < settings.sensitivity:
+                self.positive += 1
+                self.positive_infected += 1
+        for _ in range(settings.populationSize - self.infected):
+            if random.random() < 1 - settings.sensitivity:
+                self.positive += 1
+
+    def __str__(self):
+        return "Total: {}, Infected: {}, Positive: {}".format(settings.populationSize, self.infected, self.positive)
+
+
 if __name__ == "__main__":
     settings = Settings()
 
     demonstrator = Demonstrator()
-
-    pass
+    print(demonstrator)
